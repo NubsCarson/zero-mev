99a75fdf7d813a6ad07a76e2f1bb6647284d84fe
NubsCarson <nubscarson@proton.me>
2025-08-19 15:29:27 -0700

    build error fixed

--- apps/web/app/api/stream/blacklist/route.ts
+++ apps/web/app/api/stream/blacklist/route.ts
@@ -2,6 +2,8 @@ import { NextRequest } from 'next/server';
 import { getClickHouseClient } from '../../../../../../packages/db/src';
 import { env } from '@/lib/env';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(request: NextRequest) {
   const encoder = new TextEncoder();
   
